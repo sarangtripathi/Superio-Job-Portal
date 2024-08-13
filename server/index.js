3cfa3a644c11947f7b12b662827021f033921f4c
Sarang Tripathi <sarangtripathi18@gmail.com>
2024-08-13 09:48:23 +0530

    transform env

--- server/index.js
+++ server/index.js
@@ -113,8 +113,8 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on ${port}`)
-})
+});
